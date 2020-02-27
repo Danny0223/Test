79b3b7c6dd0376439928c0c7c71f6013c1ba0787
dhernandez410 <dhernandez410@csc208.csudh.edu>
2020-02-26 17:03:06 -0800

    done

---
+++ quiz1b.c
@@ -0,0 +1,21 @@
+//Danny Hernandez
+//CSC-251
+//2-26-20
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	double x =0;
+	int i;
+	double y;
+	for(i=0;i<11;i++)
+	{
+		x=x+2;
+	}
+	y=x/20;
+	printf("X/20 is %.3f\n",y);
+	printf("The value of x is %.f\n",x);
+}
+
